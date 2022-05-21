0b56c5ab13b5af312b2125705dfebb3b7928c1b7
Julia Clepf <jumgc09@gmail.com>
2022-05-21 17:57:21 -0300

    feat: add series to list

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Serie;
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index()
+    {
+        $series = Serie::all();
+
+        return view('series.index')->with('series', $series);
+    }
+
+    public function create()
+    {
+        return view(view:'series.create');
+    }
+
+
+    public function store(Request $request)
+    {
+        $serieName = $request->input(key: 'name');
+        $serie = new Serie();
+        $serie->name = $serieName;
+        $serie->save();
+
+
+        return redirect(to: '/series');
+    }
+}
