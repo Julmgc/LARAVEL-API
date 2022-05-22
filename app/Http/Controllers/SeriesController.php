bc65c9b813f099f42849a9ec3034eb255ce1871c
Julia Clepf <jumgc09@gmail.com>
2022-05-22 11:54:04 -0300

    feat: add seasons and episodes when adding series to list

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 use App\Models\Series;
+use App\Models\Season;
+use App\Models\Episode;
 use Illuminate\Http\Request;
 use App\Http\Requests\SeriesFormRequest;
 
@@ -23,7 +25,31 @@ class SeriesController extends Controller
     public function store(SeriesFormRequest $request)
     {
         $series = Series::create($request->all());
+        $seasons = [];
+    
+        for ($i = 1; $i <= $request->seasonsQty; $i++) {
+            $seasons[] = [
+                'series_id' => $series->id,
+                'number' => $i,
+            ];
+        }
+
+        Season::insert($seasons);
+
+        $episodes = [];
+        foreach ($series->seasons as $season) {
+            for ($j = 1; $j <= $request->episodes; $j++) {
+                $episodes[] = [
+                    'season_id' => $season->id,
+                    'number' => $j
+                ];
+            }
+        }
+        Episode::insert($episodes);
+
         return to_route('series.index')->with('message.success',"Series {$series->name} was added.");
+    
+        
     }
 
     public function destroy(Series $series, Request $request) 
--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,4 +1,25 @@
 <x-layout title="New Series " >
-    <x-series.form :action="route('series.store')" :name="old('name')" :update="false"/>
+    <form action="{{ route('series.store') }}" method="post">
+        @csrf
+        <div class="row mb-3">
+            <div class="col-8">
+                <label for="name" class="form-label">name:</label>
+                <input type="text" autofocus id="name" name="name" class="form-control" 
+                value="{{ old('name') }}">
+           </div>
+           <div class="col-2">
+            <label for="seasonsQty" class="form-label">Seasons:</label>
+            <input type="text" id="seasonsQty" name="seasonsQty" class="form-control" 
+            value="{{ old("seasonsQty") }}">
+            </div>
+            <div class="col-2">
+                <label for="episodes" class="form-label">Episodes per season:</label>
+                <input type="text" id="episodes" name="episodes" class="form-control" 
+                value="{{ old('episodes') }}">
+            </div>
+        </div>
+   
+        <button type="submit" class="btn btn-primary">Add</button>
+   </form>
 </x-layout>
 
